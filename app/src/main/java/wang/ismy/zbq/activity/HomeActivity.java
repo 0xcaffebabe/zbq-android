e44989218e45c17cd39a079a00b07f36cfdf87c1
My、 <34188127+0xcaffebabe@users.noreply.github.com>
2019-08-01 21:07:26 +0800

    20190801

--- app/src/main/java/wang/ismy/zbq/activity/HomeActivity.java
+++ app/src/main/java/wang/ismy/zbq/activity/HomeActivity.java
@@ -6,22 +6,38 @@ import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 import wang.ismy.zbq.R;
 import wang.ismy.zbq.databinding.ActivityHomeBinding;
 import wang.ismy.zbq.databinding.ActivityMainBinding;
+import wang.ismy.zbq.fragment.ContentFragment;
+import wang.ismy.zbq.fragment.PersonFragment;
 import wang.ismy.zbq.fragment.SocialFragment;
+import wang.ismy.zbq.fragment.StudyFragment;
 
 
 public class HomeActivity extends AppCompatActivity {
 
     private ActivityHomeBinding binding;
+
+    private List<Fragment> fragmentList =
+            Arrays.asList(new SocialFragment(),new ContentFragment(),new StudyFragment(),new PersonFragment());
+
+    private List<String> tabList = Arrays.asList("社交","内容","学习","我的");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,13 +49,65 @@ public class HomeActivity extends AppCompatActivity {
         binding.homeContainer.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
             @Override
             public Fragment getItem(int position) {
-                return new SocialFragment();
+                return fragmentList.get(position);
             }
 
             @Override
             public int getCount() {
-                return 4;
+                return fragmentList.size();
             }
         });
+        for (String s: tabList){
+            TabLayout.Tab tab = binding.homeTabLayout.newTab();
+            tab.setText(s);
+            tab.setIcon(R.drawable.compass);
+            binding.homeTabLayout.addTab(tab);
+        }
+        binding.homeTabLayout.addOnTabSelectedListener(new TabLayout.BaseOnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                binding.homeContainer.setCurrentItem(tabList.indexOf(Objects.requireNonNull(tab.getText()).toString()));
+
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        binding.homeContainer.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                TabLayout.Tab tab = binding.homeTabLayout.getTabAt(position);
+                if (tab != null){
+                    tab.select();
+                }
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+    }
+
+    public class Presenter{
+        public void menuItemClick(){
+            Toast.makeText(getApplicationContext(),"click",Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/wang/ismy/zbq/view/MenuItemView.java
+++ app/src/main/java/wang/ismy/zbq/view/MenuItemView.java
@@ -3,10 +3,12 @@ package wang.ismy.zbq.view;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import wang.ismy.zbq.R;
 
@@ -16,6 +18,7 @@ public class MenuItemView extends LinearLayout {
 
     private TextView title;
 
+    private String target;
 
     public MenuItemView(Context context) {
         super(context);
@@ -26,15 +29,31 @@ public class MenuItemView extends LinearLayout {
 
         View view = inflate(context,R.layout.view_menu_item,this);
 
+        view.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                Toast.makeText(getContext(),"click",Toast.LENGTH_SHORT).show();
+            }
+        });
+
         leftIcon = view.findViewById(R.id.menu_item_left_icon);
         title = view.findViewById(R.id.menu_item_title);
 
-        TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.MenuItemView);
+        final TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.MenuItemView);
 
 
         leftIcon.setImageDrawable(ta.getDrawable(R.styleable.MenuItemView_leftIcon));
         title.setText(ta.getString(R.styleable.MenuItemView_title));
-        ta.recycle();
+        target = ta.getString(R.styleable.MenuItemView_target);
+        view.findViewById(R.id.menu_item_container).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(),"点击事件",Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
     }
 
@@ -45,4 +64,6 @@ public class MenuItemView extends LinearLayout {
     public MenuItemView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
     }
+
+
 }
