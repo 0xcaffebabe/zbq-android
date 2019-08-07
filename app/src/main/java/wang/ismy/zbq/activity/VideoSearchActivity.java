33baa2231239774632b4a6dde6ff510614862e58
My、 <34188127+0xcaffebabe@users.noreply.github.com>
2019-08-07 21:01:55 +0800

    20190807

--- app/src/main/java/wang/ismy/zbq/activity/VideoSearchActivity.java
+++ app/src/main/java/wang/ismy/zbq/activity/VideoSearchActivity.java
@@ -1,16 +1,36 @@
 package wang.ismy.zbq.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Toast;
 
 import wang.ismy.zbq.R;
+import wang.ismy.zbq.databinding.ActivityVideoSearchBinding;
 
 public class VideoSearchActivity extends AppCompatActivity {
+
+    private ActivityVideoSearchBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_video_search);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_video_search);
+
+        binding.videoSearchSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                binding.videoSearchEngine.setText(getResources().getStringArray(R.array.video_engine)[i]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
     }
 }
